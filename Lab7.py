5516e762a77a3f4d9111fe23a6b2fc4d658a8100
VipulNandigala <vipuln@umich.edu>
2024-05-26 13:26:45 -0400

    First Submission

--- Lab7.py
+++ Lab7.py
@@ -10,9 +10,30 @@ Processes:
 Output(s):
 """
 
+
+def encrypt_message(message):
+    encrypted_message = ""
+    for char in message:
+        encrypted_message += chr(ord(char) + 3)
+    return encrypted_message
+
+
+def decrypt_message(cipher):
+    decrypted_message = ""
+    for char in cipher:
+        decrypted_message += chr(ord(char) - 3)
+    return decrypted_message
+
+
 def main():
-    pass # replace this line with all of your solution code
+    message = input("Enter the message to be encrypted: ")
+    encrypted_message = encrypt_message(message)
+    print(f"Encrypted message = {encrypted_message}\n")
+
+    cipher = input("Enter the cipher to be decrypted: ")
+    decrypted_message = decrypt_message(cipher)
+    print(f"Decrypted message = {decrypted_message}\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
